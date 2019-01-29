c36fc76630388536a828342bd1d50a24f17b8cdf
Ari Häyrinen <ari.hayrinen@jyu.fi>
2019-01-29 13:31:04 +0200

    select&delete node fix

--- gp_collections/documents_select_delete/run.js
+++ gp_collections/documents_select_delete/run.js
@@ -17,7 +17,6 @@ var select_value = settings.select.split(";");
 
 out.value = null;
 
-
 if(Array.isArray(input_value)) {
 	input_value.forEach(function(value) {
 		if(compare(value, select_value)) {
@@ -38,7 +37,7 @@ function compare (input, select) {
 	
 	for(var i = 0; i < select.length; i++) {
 		// numeric greater or smaller
-		if(settings.numeric) {
+		if(settings.numeric == 'true') {
 			input = parseFloat(input);
 			var value = parseFloat(select[i]);
 			if(settings.comparision === ">")
@@ -47,7 +46,6 @@ function compare (input, select) {
 				return (input < value ? context.doc._id : null)
 			
 		}
-		
 		if(input == select[i]) // == is intentional (user can only insert strings)
 			return context.doc._id;
 		
