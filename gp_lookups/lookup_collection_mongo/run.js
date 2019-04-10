08c3f50e99e39fd445c2ae456435457a2ee118e5
ari <arihayri@debian.arttu>
2019-04-10 15:50:30 +0300

    gp_lookups update

--- gp_lookups/lookup_collection_mongo/run.js
+++ gp_lookups/lookup_collection_mongo/run.js
@@ -3,7 +3,6 @@
 out.value = '';
 var prefix = context.node.settings.prefix;
 
-out.console.log(context.data.length)
 if(context.data) {
 	if(Array.isArray(context.data)) { 
 		// if there is 1 match, then output result as it is
@@ -14,13 +13,22 @@ if(context.data) {
 				var data = context.data[0];
 				out.setter = {};
 				
-				for (var i in Object.entries(data)) {
-					if(Array.isArray(Object.entries(data)[i][1])) {
-						// we take only the first row of data array
-						out.setter[prefix + Object.entries(data)[i][0]] = Object.entries(data)[i][1][0];
-					} else {
-						out.setter[prefix + Object.entries(data)[i][0]] = Object.entries(data)[i][1];
+				// if lookup is for one ID only, then we take all rows of fields as they are (1-1 relation)
+				if(context.node.settings.mode == 'single_id') {
+					for (var i in Object.entries(data)) {
+							out.setter[prefix + Object.entries(data)[i][0]] = Object.entries(data)[i][1];
 					}
+					
+				// if lookups is made for multiple IDs, then we take only first row of each field (get values by id)
+				} else if(context.node.settings.mode == 'multiple_id') {
+					for (var i in Object.entries(data)) {
+						if(Array.isArray(Object.entries(data)[i][1])) {
+							//we take only the first row of data array
+							out.setter[prefix + Object.entries(data)[i][0]] = Object.entries(data)[i][1][0];
+						} else {
+							out.setter[prefix + Object.entries(data)[i][0]] = Object.entries(data)[i][1];
+						}
+					}					
 				}
 			// else we just output the result
 			} else {
