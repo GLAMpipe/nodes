870f331b9b7ee2fa60f24ad34c23951111b62cec
Ari Häyrinen <ari.hayrinen@jyu.fi>
2020-01-20 17:17:49 +0200

    node rewrites

--- CSV/source_web_csv/init.js
+++ CSV/source_web_csv/init.js
@@ -29,11 +29,11 @@ var s =  date.getSeconds();
 var ss =  date.getMilliseconds();
 
 // generate filename
-var filename = y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + s + "-" + ss + ".csv";
+var filename = y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + s + "_" + ss + ".csv";
 
 // check settings
 if(context.node.settings.mode === "append" && context.node.settings.update_key == "") {
 	context.error = "You must give update_key in append mode!"
 }
-
-core.filename = funcs.path.join(context.node.project_dir, filename);
+core.filename = filename;
+core.file = funcs.path.join(context.node.project_dir, filename);
