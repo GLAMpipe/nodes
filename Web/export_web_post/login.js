bf4404b452fbbfa3deda0977d7b58528ce064189
ari <ari.hayrinen@gmail.com>
2020-04-08 17:22:57 +0300

    web export basic

--- Web/export_web_post/login.js
+++ Web/export_web_post/login.js
@@ -1,11 +1,29 @@
-var options = {
-	headers: {'content-type' : 'application/x-www-form-urlencoded'},
-	url: "https://jyx-beta.jyu.fi/rest/login", 
-	method: 'POST',
-	form: {"email": context.node.settings.username, "password": context.node.settings.password},
-	json: true
-};
 
-core.login = options;		
+// auth
+if(context.node.settings.login_type == 'basic') {
+
+	core.options = {
+		url: context.node.params.file_url,
+		method: 'GET'
+	}
+	
+	core.options.auth = {
+		'username': context.node.settings._username,
+		'password': context.node.settings._password,
+		'sendImmediately': true
+	}
+} else {
+
+	var options = {
+		headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		url: "https://jyx-beta.jyu.fi/rest/login", 
+		method: 'POST',
+		form: {"email": context.node.settings.username, "password": context.node.settings.password},
+		json: true
+	};
+
+	core.login = options;		
+}
+
 out.say("progress", "Trying to login ..."); 
 
--- Web/export_web_post/options.js
+++ Web/export_web_post/options.js
@@ -1,18 +1,36 @@
 
 
-var serie = "JYU dissertations";
-var uuid = context.doc['uuid'];
-
 //var item = context.doc[context.node.params.in_field];
 var out_link = context.doc[context.node.params.out_field];
 
-var url = "https://jyx-beta.jyu.fi/rest/collections/d0f1f10d-b003-473f-aed2-d4a88e49e5bd/items"
+// url
+var url = context.node.settings.url_start
+if(context.node.settings.url_middle && context.doc[context.node.settings.url_middle]) {
+	url += context.node.settings.url_middle
+}
+url += context.node.settings.url_end; 
+
+// parameters
+if(context.node.settings.inkey_param_1 && context.node.settings.param_1_name) {
+	var value_1 = context.doc[context.node.settings.inkey_param_1]
+	url += "?" + context.node.settings.param_1_name + "=" + value_1;
+	if(context.node.settings.inkey_param_2 && context.node.settings.param_2_name) {
+		var value_2 = context.doc[context.node.settings.inkey_param_2]
+		url += "&" + context.node.settings.param_2_name + "=" + value_2;
+	}
+}
+
+
+
+//let buff = Buffer.from(context.node.settings._username + ":" + config.data.converis_passwd);  
+//let base64cred = buff.toString('base64');
+
 
 var options = {
-	url: url,  // from init.js
+	url: url,  
 	json: true,
-	body: {uuid:uuid},
-	method: 'PUT',
+	body: {},
+	method: 'POST',
 	headers: {
 		"accept": "application/json",
 		"Content-Type": "application/json"
@@ -20,6 +38,18 @@ var options = {
 };
 
 
+// auth
+if(context.node.settings.login_type == 'basic') {
+
+	options.auth = {
+		'username': context.node.settings._username,
+		'password': context.node.settings._password,
+		'sendImmediately': true
+	}
+}
+
+
+console.log(options)
 
 // if there is an url in out_link, then we do not run again
 //if(out_link && typeof out_link == "string" && out_link.match(/^http/))
