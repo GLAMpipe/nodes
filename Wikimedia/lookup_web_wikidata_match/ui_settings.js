0324d71d86f1bcd33740f163099927babb47bab5
ari <ari.hayrinen@gmail.com>
2021-10-23 23:09:46 +0300

    reconciliation node: link fix again

--- Wikimedia/lookup_web_wikidata_match/ui_settings.js
+++ Wikimedia/lookup_web_wikidata_match/ui_settings.js
@@ -3,7 +3,7 @@
 
 
 var a = document.getElementById('process_lookup_web_wikidata_match_ui')
-a.href = `/apps/reconciliation?node=${node._id}`
+a.href = `/apps/reconciliation/?node=${node._id}`
 
 var div = document.getElementById('process_lookup_wikidata_recon_query_value')
 div.textContent = node.params.in_field
